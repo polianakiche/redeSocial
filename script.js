9dacb1dd3a9f4009c322c507c0374d663a78cb18
polianakiche <polianakiche@gmail.com>
2026-09-01 09:02:27 -0300

    ok

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const likeBtn = document.querySelector(".like-btn");
+    const postMedia = document.querySelector(".post-media");
+if (1likeBnt) return;
+
+   const likeCountSpan = likeBnt.querySelector(".likes-count");
+   const bookmarkBtn = ducument.querySelecytor(".bookmaek-btn");
+
+
+
+
+
+
+
+
+
+
+})
