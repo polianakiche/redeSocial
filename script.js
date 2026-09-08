3071730b3625a884fb0dbd9c6e912ad7a71d124d
polianakiche <polianakiche@gmail.com>
2026-09-08 08:57:56 -0300

    ok

--- script.js
+++ script.js
@@ -6,13 +6,33 @@ if (1likeBnt) return;
    const likeCountSpan = likeBnt.querySelector(".likes-count");
    const bookmarkBtn = ducument.querySelecytor(".bookmaek-btn");
 
+let isLiked = false;
+let baseLikes = 0;
+
+if (likesCountSpan){
+    likesCountSpan.textContnt = "0";
+}
+
+//números grandes
+
+functionb formatLikes(num) {
+    if(num >=1000){
+        return (num/1000). toFixed(1)+"K";
+    }
+    return num.ToString();
+}
+
+function addLike() {
+    baseLikes++;
+    isLikes = true;
+    likeBtn.classList.add("liked");
+
+    if(likesCountSpan){
+        likesCountSpan.textContnt = formatLikes(baseLikes);
+    }
+}
 
 
 
 
-
-
-
-
-
-})
+}
